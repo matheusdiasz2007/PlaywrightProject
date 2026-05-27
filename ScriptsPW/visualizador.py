7b07d14c9337696b9bfea6edbe396af9742323c8
Matheus Dias Pereira <matheus108532@gmail.com>
2026-05-27 02:12:26 -0300

    Add files via upload

---
+++ ScriptsPW/visualizador.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright
+from time import sleep
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context(
+        color_scheme='dark',
+        **p.devices['iPhone 14'],
+    )
+    page = context.new_page()
+    page.goto('https://matheusdiasz2007.github.io/Portfolio-Site/')
+    print(page.title())
+
+    sleep(60)
+
+    browser.close()
